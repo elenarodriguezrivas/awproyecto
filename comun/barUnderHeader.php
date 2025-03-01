32f815e5e9dceb20c04919a288cb496990817901
JDwanW <149677321+JDwanW@users.noreply.github.com>
2025-03-01 14:19:15 +0100

    V2.1 Interfaz

--- comun/barUnderHeader.php
+++ comun/barUnderHeader.php
@@ -1,9 +1,9 @@
 <nav id="barUnderHeader">
     <ul class="nav-links">
         <li><a href="index.php">Inicio</a></li> 
-        <li><a href="view/catalogo_pantalla.php">Productos</a></li>
-        <li><a href="view/subasta_pantalla.php">Mis Productos</a></li>
-        <li><a href="view/subasta_pantalla.php">Subastas</a></li>
-        <li><a href="index.html">Perfil</a></li>
+        <li><a href="catalogo_pantalla.php">Productos</a></li>
+        <li><a href="micatalogo_pantalla.php">Mis Productos</a></li>
+        <li><a href="subasta_pantalla.php">Subastas</a></li>
+        <li><a href="perfil_pantalla.php">Perfil</a></li>
     </ul>
 </nav>
