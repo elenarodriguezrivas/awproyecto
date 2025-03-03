f41bdcef17f7c1b78e7769e175c3981d6f77f66b
yayayajia <129692232+yayayajia@users.noreply.github.com>
2025-03-03 14:55:41 +0100

    inicio

--- comun/barUnderHeader.php
+++ comun/barUnderHeader.php
@@ -1,6 +1,6 @@
 <nav id="barUnderHeader">
     <ul class="nav-links">
-        <li><a href="index.php">Inicio</a></li>
+        <li><a href="/awproyecto/index.php">Inicio</a></li>
         <li><a href="view/catalogo_pantalla.php">Productos</a></li>
         <li><a href="view/micatalogo_pantalla.php">Mis Productos</a></li>
         <li><a href="view/subasta_pantalla.php">Subastas</a></li>
