ba804f0ef5b88b7704c920eaf91e9e494ad69297
Elena RR <147281689+elenarodriguezrivas@users.noreply.github.com>
2025-03-03 11:46:05 +0100

    Corrección de enlaces

--- comun/cabecera.php
+++ comun/cabecera.php
@@ -12,5 +12,5 @@ function mostrarSaludo() {
 <header>
     <h1>MercaSwapp</h1>
     <p class="tagline">Tecnología Circular • Subastas Dinámicas • Trueque Inteligente</p>
-    <img src="../comun/img/logo-mercaswapp.png" alt="Logo MercaSwapp" class="logo">
+    <img src="comun/img/logo-mercaswapp.png" alt="Logo MercaSwapp" class="logo">
 </header>
