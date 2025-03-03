760cbc8f74c0684d03f2500cf1682bc558f04c4e
yayayajia <129692232+yayayajia@users.noreply.github.com>
2025-03-03 15:41:02 +0100

    como?

--- comun/barUnderHeader.php
+++ comun/barUnderHeader.php
@@ -1,17 +1,9 @@
 <nav id="barUnderHeader">
     <ul class="nav-links">
-<<<<<<< HEAD
-        <li><a href="index.php">Inicio</a></li>
-        <li><a href="view/catalogo_pantalla.php">Productos</a></li>
-        <li><a href="view/micatalogo_pantalla.php">Mis Productos</a></li>
-        <li><a href="view/subasta_pantalla.php">Subastas</a></li>
-        <li><a href="view/perfil_pantalla.php">Perfil</a></li>
-=======
         <li><a href="index.php">Inicio</a></li>
         <li><a href="catalogo_pantalla.php">Productos</a></li>
         <li><a href="micatalogo_pantalla.php">Mis Productos</a></li>
         <li><a href="subasta_pantalla.php">Subastas</a></li>
         <li><a href="perfil_pantalla.php">Perfil</a></li>
->>>>>>> 63226fa719e5a2705ac31cbd171aab85bcf0c4d4
     </ul>
 </nav>
