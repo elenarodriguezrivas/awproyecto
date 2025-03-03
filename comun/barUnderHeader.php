2a7c558eb5ec9a7f07436ce95f3a9a0436583607
yayayajia <129692232+yayayajia@users.noreply.github.com>
2025-03-03 15:48:16 +0100

    conecciones

--- comun/barUnderHeader.php
+++ comun/barUnderHeader.php
@@ -1,9 +1,9 @@
 <nav id="barUnderHeader">
     <ul class="nav-links">
-        <li><a href="index.php">Inicio</a></li>
-        <li><a href="catalogo_pantalla.php">Productos</a></li>
-        <li><a href="micatalogo_pantalla.php">Mis Productos</a></li>
-        <li><a href="subasta_pantalla.php">Subastas</a></li>
-        <li><a href="perfil_pantalla.php">Perfil</a></li>
+        <li><a href="/awproyecto/index.php">Inicio</a></li>
+        <li><a href="/awproyecto/view/catalogo_pantalla.php">Productos</a></li>
+        <li><a href="/awproyecto/view/micatalogo_pantalla.php">Mis Productos</a></li>
+        <li><a href="/awproyecto/view/subasta_pantalla.php">Subastas</a></li>
+        <li><a href="/awproyecto/view/perfil_pantalla.php">Perfil</a></li>
     </ul>
 </nav>
