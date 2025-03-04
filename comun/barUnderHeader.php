a275f8e52396ff1e666774ab08256c3a9b3c9d5a
alejremi <alejremi@ucm.es>
2025-03-04 15:18:57 +0100

    funcionando la barra

--- comun/barUnderHeader.php
+++ comun/barUnderHeader.php
@@ -1,9 +1,19 @@
 <nav id="barUnderHeader">
     <ul class="nav-links">
-        <li><a href="/awproyecto/index.php">Inicio</a></li>
-        <li><a href="/awproyecto/view/catalogo_pantalla.php">Productos</a></li>
-        <li><a href="/awproyecto/view/micatalogo_pantalla.php">Mis Productos</a></li>
-        <li><a href="/awproyecto/view/subasta_pantalla.php">Subastas</a></li>
-        <li><a href="/awproyecto/view/perfil_pantalla.php">Perfil</a></li>
+        <div class="center">
+            <li><a href="/awproyecto/index.php">Inicio</a></li>
+            <li><a href="/awproyecto/view/catalogo_pantalla.php">Productos</a></li>
+            <li><a href="/awproyecto/view/micatalogo_pantalla.php">Mis Productos</a></li>
+            <li><a href="/awproyecto/view/subasta_pantalla.php">Subastas</a></li>
+            <li><a href="/awproyecto/view/perfil_pantalla.php">Perfil</a></li>
+        </div>
+        <div class="right">
+            <?php if (isset($_SESSION['login'])): ?>
+                <li><a href="/awproyecto/view/logout_pantalla.php">Cerrar sesión</a></li>
+            <?php else: ?>
+                <li><a href="/awproyecto/view/login_pantalla.php">Login</a></li>
+                <li><a href="/awproyecto/view/register_pantalla.php">Registrarse</a></li>
+            <?php endif; ?>
+        </div>
     </ul>
 </nav>
--- comun/cabecera.php
+++ comun/cabecera.php
@@ -15,17 +15,6 @@ function mostrarSaludo() {
     <img src="/awproyecto/comun/img/logo-mercaswapp.png" alt="Logo MercaSwapp" class="logo">
     <nav id="barUnderHeader">
         <ul class="nav-links">
-            <li><a href="/awproyecto/index.php">Inicio</a></li>
-            <li><a href="/awproyecto/view/catalogo_pantalla.php">Productos</a></li>
-            <li><a href="/awproyecto/view/micatalogo_pantalla.php">Mis Productos</a></li>
-            <li><a href="/awproyecto/view/subasta_pantalla.php">Subastas</a></li>
-            <li><a href="/awproyecto/view/perfil_pantalla.php">Perfil</a></li>
-            <?php if (isset($_SESSION['login'])): ?>
-                <li><a href="/awproyecto/view/logout_pantalla.php">Cerrar sesión</a></li>
-            <?php else: ?>
-                <li><a href="/awproyecto/view/login_pantalla.php">Login</a></li>
-                <li><a href="/awproyecto/view/register_pantalla.php">Registrarse</a></li>
-            <?php endif; ?>
         </ul>
     </nav>
 </header>
