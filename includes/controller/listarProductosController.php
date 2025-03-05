c2cbbe8ec0303a98ceaad0e3727e6be18dddeccb
alejremi <alejremi@ucm.es>
2025-03-05 18:29:23 +0100

    controller

--- includes/controller/listarProductosController.php
+++ includes/controller/listarProductosController.php
@@ -10,32 +10,17 @@ class ListarProductosController {
     }
 
     public function listarProductos() {
-        
+        $arrayProductos = $this->listarProductoSA->listarProductos();
     }
 }
 
-if (!isset($_SESSION['userid'])) {
-    echo json_encode(["error" => "Usuario no autenticado."]);
-    exit();
+for($i = 0; count($arrayProductos); $i++){
+    $producto = $arrayProductos[$i];
+    $productos[] = [
+        "nombre" => $ptoducto->getNombreProducto(),
+        "descripcion" => $producto->getDescripcionProducto()
+    ]
 }
-
-$controller = new ObtenerPerfilController();
-$user = $controller->mostrarPerfil($_SESSION['userid']);
-
-if (!$user) {
-    echo json_encode(["error" => "Usuario no encontrado."]);
-    exit();
-}
-
-function pixelarCorreo($correo) {
-    $partes = explode("@", $correo);
-    $inicio = substr($partes[0], 0, 2) . str_repeat("*", max(0, strlen($partes[0]) - 2));
-    return $inicio . "@" . $partes[1];
-}
-
-$correoPixelado = pixelarCorreo($user->getEmail());
-
-//se usará para las views, trabajamos con jsons
 echo json_encode([
     "nombre" => $user->getNombre(),
     "apellidos" => $user->getApellidos(),
