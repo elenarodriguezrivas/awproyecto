8c7bb747ac247632056513e6ccbfba3a507255cb
Elena RR <147281689+elenarodriguezrivas@users.noreply.github.com>
2025-03-05 10:00:42 +0100

    Perfil
    Ayer deciais que a vosotros sí os funcionaba. Lo voy a dejar como esta y me pongo con otra cosa

--- view/perfil_pantalla.php
+++ view/perfil_pantalla.php
@@ -1,37 +1,29 @@
 <?php
-require_once __DIR__ . '/../controller/obtenerPerfilController.php';
 
 $tituloPagina = 'Perfil';
-
-// Evitar que la vista se cargue si no hay usuario
-if (!$user) {
-    $contenidoPrincipal = "<p>Error: No se pudo cargar el perfil.</p>";
-} else {
-    $contenidoPrincipal = <<<EOS
+		
+$contenidoPrincipal=<<<EOS
     <section class="presentacion">
-        <h2>Mi Perfil</h2>
-        <div class="destacado">
-            <ul>
-                <div class="destacado">
-                    <img src="$imagenPerfil" alt="Foto de perfil" width="150" height="150">
-                    <p> <label for="nombre">Nombre: {$user['nombre']}</label> </p>
-                    <p> <label for="1rApellido">1r Apellido: {$user['apellidos']}</label></p>
-                    <p> <label for="Edad">Edad: {$user['edad']}</label> </p>
-                    <p> <label for="Correo">Correo: {$correoPixelado}</label> </p> 
-                    <p> <label for="Saldo">Saldo: {$user['saldo']} €</label> </p> 
+    <h2>Mi Perfil</h2>
+            <div class="destacado">
+                <ul>
+                    <div class="destacado">
+                    <p> <label for="nombre">Nombre: que lo pille de bbdd</label> </p>
+                    <p> <label for="1rApellido">1r Apellido: que lo pille de bbdd</label> 
+                    <label for="2oApellido">2o Apellido*: que lo pille de bbdd</label> </p>
+                    <p> <label for="Edad">Edad: que lo pille de bbdd</label> </p>
+                    <p> <label for="Correo">Correo: que salga pixelado los primeros chars</label> </p> 
                     <p> <a href="modificarperfil_pantalla.php"><button type="button">Cambiar Datos</button></a> </p>
 
                     <p class="descripcion-breve"><em>* : Opcional </em></p>
-                </div>
-            </ul>
-        </div>
+                </ul>
+            </div>
                             
-        <p class="descripcion-breve"><em>Plataforma segura</em> que combina innovación tecnológica con responsabilidad ambiental. 
-        Ofrecemos un espacio donde cada transacción contribuye a reducir residuos electrónicos mientras 
-        disfrutas de experiencias de compra únicas. <strong>¡Únete a la revolución circular!</strong></p>
+            <p class="descripcion-breve"><em>Plataforma segura</em> que combina innovación tecnológica con responsabilidad ambiental. 
+            Ofrecemos un espacio donde cada transacción contribuye a reducir residuos electrónicos mientras 
+            disfrutas de experiencias de compra únicas. <strong>¡Únete a la revolución circular!</strong></p>
     </section>
 EOS;
-}
 
-require_once __DIR__ . '/../comun/plantilla.php';
+require("../comun/plantilla.php");
 ?>
