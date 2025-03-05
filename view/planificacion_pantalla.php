5b86edbc69a720e009d3279f96e471e4806f88ef
VickyMM03 <mmagpali@ucm.es>
2025-03-05 14:16:38 +0100

    creado planificacion pantalla del pie de la pagina

---
+++ view/planificacion_pantalla.php
@@ -0,0 +1,11 @@
+<?php
+$contenidoPrincipal=<<<EOS
+    <section class="presentacion">
+    <h2>Catalogo de Productos</h2>
+            
+    </section>
+EOS;
+
+require_once __DIR__ . '/../comun/plantilla.php';
+
+?>
