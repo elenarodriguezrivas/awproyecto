a8ba1aa7e662e4add4829e7c08a01ba7d574f613
Elena RR <147281689+elenarodriguezrivas@users.noreply.github.com>
2025-03-05 18:16:36 +0100

    SAProductos

--- includes/Producto/sa/buscarProductosPorCategoriaSA.php
+++ includes/Producto/sa/buscarProductosPorCategoriaSA.php
@@ -2,15 +2,15 @@
 require_once __DIR__ . '/../dao/ProductoDAO.php';
 require_once __DIR__ . '/../model/Producto.php';
 
-class CrearProductoSA {
+class buscarProductosPorCategoriaSA {
     private $productoDAO;
 
     public function __construct() {
         $this->productoDAO = new ProductoDAO();
     }
 
-    public function agregarProducto(Producto $producto): bool {
-        return $this->productoDAO->agregarProducto($producto);
+    public function buscarProductosPorCategoria(string $categoria): array{
+        return $this->productoDAO->buscarProductosPorCategoria($categoria);
     }
     
 }
