218cca8ada35ac552392af87e39e82ff61f8f630
VickyMM03 <mmagpali@ucm.es>
2025-03-05 14:12:47 +0100

    añadir modificarperfil en perfil

--- view/perfil_pantalla.php
+++ view/perfil_pantalla.php
@@ -15,6 +15,8 @@ $contenidoPrincipal = <<<EOS
         <p class="perfil-item">Apellidos: <span id="apellidos" class="perfil-dato"></span></p>
         <p class="perfil-item">Edad: <span id="edad" class="perfil-dato"></span></p>
         <p class="perfil-item">Correo: <span id="correo" class="perfil-dato"></span></p>
+        <a href="modificarperfil_pantalla.php"><button>Modificar perfil</button></a>
+
     </div>
 
     <script src="JS/perfilJS.js"></script>
