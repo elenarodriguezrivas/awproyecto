3e8bfcab873eb41599c968dae966e4acb0e2551d
Elena RR <147281689+elenarodriguezrivas@users.noreply.github.com>
2025-03-06 13:58:11 +0100

    eliminarProducto

--- view/eliminarProducto_pantalla.php
+++ view/eliminarProducto_pantalla.php
@@ -2,7 +2,12 @@
 $tituloPagina = 'Eliminar un producto';
 
 $contenidoPrincipal = <<<EOS
-
+<form method="post" action="eliminarProducto.php">
+    <label for="nombreProducto">Nombre del Producto:</label>
+    <input type="text" id="nombreProducto" name="nombreProducto" required>
+    <button type="submit">Eliminar Producto</button>
+</form>
+<script src="JS/eliminarProductoJS.js"></script>
 EOS;
 
 require_once __DIR__ . "/../comun/plantilla.php";
