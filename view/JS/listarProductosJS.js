80e27521fd9efc94c04de6fb129357e5430b8100
alejremi <alejremi@ucm.es>
2025-03-25 17:43:44 +0100

    Cambiado ListarProductosJS.js

--- view/JS/listarProductosJS.js
+++ view/JS/listarProductosJS.js
@@ -1,33 +1,39 @@
-$(document).ready(function() {
-    $.ajax({
-        url: '../includes/controller/obtenerProductosController.php',
-        method: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            if (data.length === 0) {
-                $('#productos').html('<p>No hay productos disponibles.</p>');
-            } else {
-                var productosHtml = '';
-                data.forEach(function(producto) {
-                    productosHtml += '<div class="producto">';
-                    productosHtml += '<h3>' + producto.nombreProducto + '</h3>';
-                    productosHtml += '<p>' + producto.descripcionProducto + '</p>';
-                    productosHtml += '<p>Precio: ' + producto.precio + '€</p>';
-                    productosHtml += '<p>Categoría: ' + producto.categoriaProducto + '</p>';
-                    productosHtml += '<img src="../' + producto.rutaImagen + '" style="height: 200px;" />';
-                    /*PARA LA PRACTICA 3 
-                    productosHtml += '<form action="../includes/controller/ComprarProductoController.php" method="POST">';
-                    productosHtml += '<input type="hidden" name="producto_id" value="' + producto.id + '">';
-                    productosHtml += '<input type="hidden" name="precio" value="' + producto.precio + '">';
-                    productosHtml += '<button type="submit" class="btn">Comprar</button>'; PARA LA PRACTICA 3*/
-                    productosHtml += '</form>';
-                    productosHtml += '</div>';
-                });
-                $('#productos').html(productosHtml);
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('../includes/controller/obtenerProductosController.php')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al obtener los productos');
             }
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.error('Error al obtener los productos:', textStatus, errorThrown);
-        }
-    });
+            return response.json();
+        })
+        .then(data => {
+            const productosContainer = document.getElementById('productos');
+
+            if (data.length === 0) {
+                productosContainer.innerHTML = '<p>No hay productos disponibles.</p>';
+            } else {
+                let productosHtml = '';
+                data.forEach(producto => {
+                    productosHtml += `
+                        <div class="producto">
+                            <h3>${producto.nombreProducto}</h3>
+                            <p>${producto.descripcionProducto}</p>
+                            <p>Precio: ${producto.precio}€</p>
+                            <p>Categoría: ${producto.categoriaProducto}</p>
+                            <img src="../${producto.rutaImagen}" style="height: 200px;" />
+                            <!-- PARA LA PRACTICA 3 -->
+                            <!--
+                            <form action="../includes/controller/ComprarProductoController.php" method="POST">
+                                <input type="hidden" name="producto_id" value="${producto.id}">
+                                <input type="hidden" name="precio" value="${producto.precio}">
+                                <button type="submit" class="btn">Comprar</button>
+                            </form>
+                            -->
+                        </div>
+                    `;
+                });
+                productosContainer.innerHTML = productosHtml;
+            }
+        })
+        .catch(error => console.error('Error al obtener los productos:', error));
 });
