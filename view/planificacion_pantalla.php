2a7f168a6eb7849370fce1278975790a8e8db366
alejremi <alejremi@ucm.es>
2025-03-26 12:41:10 +0100

    Tengo que terminarlo

--- view/planificacion_pantalla.php
+++ view/planificacion_pantalla.php
@@ -11,12 +11,12 @@ $contenidoPrincipal = <<<EOS
         <h3>Entregas a realizar</h3>
         <p class="descripcion-breve">En la planificación interna del proyecto, se tuvo en cuentas las fechas de entrega propuestas por el 
             profesor de la asignatura <em>Humberto Javier</em>. Se muestran a continuación todas ellas, incluidas las fechas de los ejercicios en pareja.</p>
-        <planimg><img src="<?= RUTA_IMGS ?>/fechas_planificacion.png" alt="fechas" ></planimg>
+        <planimg><img src="/../comun/img/fechas_planificacion.png" alt="fechas" ></planimg>
         <h3>Herramientas utilizadas para la elaboración del proyecto</h3>
         <p class="descripcion-breve">Se va a hacer uso de herramientas como <em>GitHub Desktop</em> para agilizar el desarrollo de las prácticas y poder hacer un registro del control de cambios...</p>
         <h3>Diagrama de Gantt</h3>
         <p>Se hace uso del siguiente diagrama de Gantt como herramienta de gestión de proyectos</p>
-        <planimg><img src="<?= RUTA_IMGS ?>/ganttd.png" alt="gantt"> </planimg>
+        <planimg><img src="/../comun/img/ganttd.png" alt="gantt"> </planimg>
         <p>En este caso, las entregas no se solapan porque primero terminamos una y empezamos la siguiente. La entrega 2 depende de la terminación de la entrega 1. </p>
     </section>
 EOS;
