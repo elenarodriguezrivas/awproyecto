8971ec97f760bcaac97e826e8ced4866b8626cd2
yayayajia <129692232+yayayajia@users.noreply.github.com>
2025-04-01 16:15:08 +0200

    funcionando ruta automatica

--- includes/config.php
+++ includes/config.php
@@ -11,10 +11,21 @@ define('BD_USER', 'awproyecto');
 define('BD_PASS', 'awproyecto');
 
 /**
+ * Detectar automáticamente la ruta base de la aplicación
+ */
+$rutaServidor = dirname($_SERVER['SCRIPT_NAME']);
+// Si estamos en un subdirectorio del servidor, ajustamos la ruta
+if (basename($rutaServidor) == 'includes' || basename($rutaServidor) == 'view') {
+    $rutaServidor = dirname($rutaServidor);
+}
+// Aseguramos que la ruta termina con '/'
+$rutaServidor = rtrim($rutaServidor, '/');
+
+/**
  * Parámetros de configuración utilizados para generar las URLs y las rutas a ficheros en la aplicación
  */
 define('RAIZ_APP', __DIR__);
-define('RUTA_APP', '/awproyecto');
+define('RUTA_APP', $rutaServidor);
 define('RUTA_IMGS', RUTA_APP.'/comun/img');
 define('RUTA_CSS', RUTA_APP.'/view/styles');
 define('RUTA_JS', RUTA_APP.'/view/JS');
