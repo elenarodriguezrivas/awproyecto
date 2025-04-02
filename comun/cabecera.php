285410424688adad35c77f94b6281177c74bd3fd
JDwanW <149677321+JDwanW@users.noreply.github.com>
2025-04-02 17:14:45 +0200

    finish cabecera

--- comun/cabecera.php
+++ comun/cabecera.php
@@ -12,20 +12,23 @@
 ?>
 
 <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <!--none(no muestra), block(muestra) -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark d-none d-sm-block">
         <div class = "row align-items-center w-100 h-100 d-none d-sm-flex">
-            <div class = "col-sm-6 col-md-2 text-left d-none d-sm-block"><h1>MercaSwapp </h1></div>
-            <!--EN DISPOSITIVOS MOVILES DESAPARECEN-->
-            <!--none(no muestra), block(muestra) -->
-            <!-- Texto para pantallas mayores a md -->
-            <div class="col-md-8 text-center d-none d-lg-block">
+            <div class = "col-sm-4 col-lg-2 text-left d-none d-sm-block">
+                <h2>MercaSwapp</h2>
+            </div>
+            <!-- Texto para pantallas lg -->
+            <div class="col-lg-8 text-center d-none d-xl-block">
                 <h3>Tecnología Circular • Subastas Dinámicas • Trueque Inteligente</h3>
             </div>
-            <!-- Texto para pantallas sm-md-->
-            <div class="col-md-8 text-center d-none d-sm-block d-lg-none">
-                <h3>Tecnología Circular</h3>
+            <div class = "col-lg-2 text-right d-none d-xl-block" >
+                <img src="<?= RUTA_IMGS ?>/logo-mercaswapp.png" alt="Logo MercaSwapp"  class="img-fluid" width="30%"> 
             </div>
-            <div class = "col-sm-6 col-md-2 text-right d-none d-md-block" ><img src="<?= RUTA_IMGS ?>/logo-mercaswapp.png" alt="Logo MercaSwapp"  class="img-fluid" width="30%"> </div>
+            <!-- Texto para pantallas sm-md-->
+            <div class="col-sm-8 text-center d-none d-sm-block d-xl-none">
+                <h2>Tecnología Circular</h2>
+            </div>
         </div>
     </nav>
 </header>
