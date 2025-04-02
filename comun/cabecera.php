ac96534b10c018c8f0e73980be59079f3c737253
JDwanW <149677321+JDwanW@users.noreply.github.com>
2025-04-02 17:14:45 +0200

    preChanges.4 bastante avanzado

--- comun/cabecera.php
+++ comun/cabecera.php
@@ -12,12 +12,20 @@
 ?>
 
 <header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class = "row align-items-center">
-        <div class = "col-md-2 text-left"><h1>MercaSwapp </h1></div>
-        <!--EN DISPOSITIVOS MOVILES DESAPARECEN-->
-        <div class = "col-md-8 text-center d-none d-sm-block"><h3> Tecnología Circular • Subastas Dinámicas • Trueque Inteligente </h3></div> 
-        <div class = "col-md-2 text-right d-none d-sm-block"><img src="<?= RUTA_IMGS ?>/logo-mercaswapp.png" alt="Logo MercaSwapp"  class="img-fluid" width="30%"> </div>
-    </div>
-</nav>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class = "row align-items-center w-100 h-100 d-none d-sm-flex">
+            <div class = "col-sm-6 col-md-2 text-left d-none d-sm-block"><h1>MercaSwapp </h1></div>
+            <!--EN DISPOSITIVOS MOVILES DESAPARECEN-->
+            <!--none(no muestra), block(muestra) -->
+            <!-- Texto para pantallas mayores a md -->
+            <div class="col-md-8 text-center d-none d-lg-block">
+                <h3>Tecnología Circular • Subastas Dinámicas • Trueque Inteligente</h3>
+            </div>
+            <!-- Texto para pantallas sm-md-->
+            <div class="col-md-8 text-center d-none d-sm-block d-lg-none">
+                <h3>Tecnología Circular</h3>
+            </div>
+            <div class = "col-sm-6 col-md-2 text-right d-none d-md-block" ><img src="<?= RUTA_IMGS ?>/logo-mercaswapp.png" alt="Logo MercaSwapp"  class="img-fluid" width="30%"> </div>
+        </div>
+    </nav>
 </header>
