deeaf70ae88379fb9e6bcbdc83ee4b0b0debf49b
yayayajia <129692232+yayayajia@users.noreply.github.com>
2025-04-03 01:40:29 +0200

    Reapply "Merge pull request #17 from elenarodriguezrivas/modificarperfil"
    This reverts commit 0a63518cb70a6b3a8d80c27db76437c386c91c38.

--- includes/config.php
+++ includes/config.php
@@ -5,9 +5,9 @@
 /**
  * Parámetros de conexión a la BD
  */
-define('BD_HOST', '192.168.1.150');
+define('BD_HOST', 'localhost');
 define('BD_NAME', 'awproyecto');
-define('BD_USER', 'root');
+define('BD_USER', 'awproyecto');
 define('BD_PASS', 'awproyecto');
 
 /**
