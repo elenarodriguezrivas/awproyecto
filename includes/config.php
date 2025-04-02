8590f71c895aa60f6825cc86f576f8b67b8b0153
JDwanW <jundwang@ucm.es>
2025-04-02 17:13:58 +0200

    preChanges

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@
 /**
  * Parámetros de conexión a la BD
  */
-define('BD_HOST', '192.168.1.150');
+define('BD_HOST', 'localhost');
 define('BD_NAME', 'awproyecto');
 define('BD_USER', 'admin');
 define('BD_PASS', 'admin');
