60f8ce236dd0d7e55dc84f037dfb4f3c951bfc54
JDwanW <149677321+JDwanW@users.noreply.github.com>
2025-04-07 11:56:48 +0200

    preChanges.4 bastante avanzado

--- comun/pie.php
+++ comun/pie.php
@@ -1,4 +1,21 @@
 <footer>
-    <p>© 2025 MercaSwapp - Grupo 6</p>
-    <p><a href="<?= RUTA_APP ?>/view/contacto_pantalla.php">Nuestro equipo</a> | <a href="<?= RUTA_APP ?>/view/planificacion_pantalla.php">Nuestra planificacion</a></p>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-custom-height">
+        <!-- Visible solo en pantallas sm o más grandes -->
+        <div class = "row align-items-center w-100 h-100 d-none d-sm-flex">
+            <div class = "col-6 text-center">© 2025 MercaSwapp - Grupo 6</div>
+            <div class = "col-6 text-center">
+                <a href="<?= RUTA_APP ?>/view/contacto_pantalla.php">Nuestro equipo</a> | 
+                <a href="<?= RUTA_APP ?>/view/planificacion_pantalla.php">Nuestra planificacion</a>
+            </div>
+        </div>
+        <!-- HACER TODO QUE SEA IGUAL QUE EL XS SOLO QUE SI NO ES XS LE MOSTRAREMOS UN PAR DE ICONOS -->
+        <!-- Visible solo en pantallas xs -->
+        <div class = "row align-items-center justify-content-center text-center w-100 h-100 d-block d-sm-none">
+            <p> 
+                <a href="<?= RUTA_APP ?>/view/contacto_pantalla.php">Nuestro equipo</a> | 
+                <a href="<?= RUTA_APP ?>/view/planificacion_pantalla.php">Nuestra planificacion</a> 
+            </p>
+            <strong>© 2025 MercaSwapp - Grupo 6</strong>
+        </div>
+    </nav>
 </footer>
