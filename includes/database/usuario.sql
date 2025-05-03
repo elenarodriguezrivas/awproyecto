ef1a5ea75c7254d01c61463e73fd01fe8a5aa822
yayayajia <129692232+yayayajia@users.noreply.github.com>
2025-05-03 11:11:31 +0200

    comentario

--- includes/database/usuario.sql
+++ includes/database/usuario.sql
@@ -10,6 +10,7 @@ CREATE TABLE Usuarios (
 
 INSERT INTO Usuarios (userid, contrasena, email, nombre, apellidos, edad, rol) 
 VALUES
+--la contraseña: vendedor1, vendedor2, vendedor3
 ('vendedor1', '$2y$10$XrvoiRarSpb/TB/YldgM4eoQUO.8iW7Xs4MB4QX5Zzd2GC89Gvwqe', 'vendedor1@example.com', 'Juan', 'Pérez', 30, 'usuario'),
 ('vendedor2', '$2y$10$92WPTjQkORMGAoGlu5FvFuUGescNf0ZUL.VvbqGTyxEvSwc7L5fpu', 'vendedor2@example.com', 'María', 'Gómez', 28, 'usuario'),
 ('vendedor3', '$2y$10$2qxmXLlBu.ZuSyb7iPamwuGLa7l24QesyPJ91SbpJVR/F0QIW7fgK', 'vendedor3@example.com', 'Carlos', 'López', 35, 'usuario');
