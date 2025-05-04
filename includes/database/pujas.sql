5676458c2736fbc507410d3f38331396c4dd2094
Elena RR <147281689+elenarodriguezrivas@users.noreply.github.com>
2025-05-04 23:24:57 +0200

    Cambio de estructura en la tabla de Pujas, clave de identificacion de tres campos. No permite que el mismo usuario puje por el mismo producto con el mismo precio

--- includes/database/pujas.sql
+++ includes/database/pujas.sql
@@ -1,11 +1,8 @@
 CREATE TABLE Pujas (
-    id INT AUTO_INCREMENT, 
     idProducto INT, 
     idPujador VARCHAR(50), 
     precio DECIMAL(10,2) NOT NULL,
-    PRIMARY KEY (idProducto, idPujador, precio),
+    PRIMARY KEY (idProducto, idPujador, precio), --no permite que el mismo usuario puje por el mismpo producto con el mismo precio
     FOREIGN KEY (idProducto) REFERENCES Productos(id) ON DELETE CASCADE, 
     FOREIGN KEY (idPujador) REFERENCES Usuarios(userid) ON DELETE CASCADE
 );
-
-
