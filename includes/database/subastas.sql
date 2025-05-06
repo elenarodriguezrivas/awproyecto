a5f27059f62a1777a319087b531966a953184ff6
Elena RR <147281689+elenarodriguezrivas@users.noreply.github.com>
2025-05-06 16:54:41 +0200

    Redefinicion tabla subastas

--- includes/database/subastas.sql
+++ includes/database/subastas.sql
@@ -1,8 +1,12 @@
 CREATE TABLE Subastas (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    idProducto INT UNIQUE NOT NULL,
+    nombreSubasta VARCHAR(255) NOT NULL,
+    descripcionSubasta TEXT NOT NULL,
+    precio_original DECIMAL(10,2) NOT NULL,
+    precio_actual  DECIMAL(10,2) NOT NULL,
+    idVendedor VARCHAR(50) NOT NULL,
+    rutaImagen VARCHAR(255) NOT NULL,
+    estado ENUM('en_subasta','anulada','finalizada') DEFAULT 'en_subasta',
     fechaSubasta DATE NOT NULL,
-    estado ENUM('en_subasta', 'vendido', 'anulada') DEFAULT 'en_subasta',
-    precio DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (idProducto) REFERENCES Productos(id) ON DELETE CASCADE
+    horaSubasta TIME NOT NULL,
 );
