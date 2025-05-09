4c8de442d31a7a13f89f2b0cbb9cba8f189e41b0
yayayajia <129692232+yayayajia@users.noreply.github.com>
2025-05-09 15:32:16 +0200

    actualizar precios

--- includes/database/productos.sql
+++ includes/database/productos.sql
@@ -12,11 +12,11 @@ CREATE TABLE Productos (
 
 INSERT INTO Productos (nombreProducto, descripcionProducto, precio, categoriaProducto, idVendedor, rutaImagen, estado) 
 VALUES 
-('Laptop ACER', 'Laptop ACER con procesador Intel i5 y 8GB de RAM.', 599.99, 'Computadora', 'vendedor1', '/fotos/producto8.jpg', 'enventa'),
-('Auriculares Sony', 'Auriculares inalámbricos con cancelación de ruido.', 199.99, 'Auriculares', 'vendedor2', '/fotos/producto7.jpg', 'enventa'),
-('Teclado Mecánico', 'Teclado mecánico rosa.', 89.99, 'Teclado', 'vendedor3', '/fotos/producto6.jpg', 'enventa'),
-('Monitor Samsung', 'Monitor Full HD de 24 pulgadas.', 149.99, 'Pantalla', 'vendedor1', '/fotos/producto2.jpg', 'enventa'),
-('Ratón', 'Ratón inalámbrico color negro. ', 79.99, 'Ratón', 'vendedor3', '/fotos/producto5.jpg', 'enventa'),
-('Teclado Mecánico', 'Teclado mecánico negro.', 89.99, 'Teclado', 'vendedor3', '/fotos/producto4.jpg', 'enventa'),
+('Laptop ACER', 'Laptop ACER con procesador Intel i5 y 8GB de RAM.', 11.99, 'Computadora', 'vendedor1', '/fotos/producto8.jpg', 'enventa'),
+('Auriculares Sony', 'Auriculares inalámbricos con cancelación de ruido.', 9.99, 'Auriculares', 'vendedor2', '/fotos/producto7.jpg', 'enventa'),
+('Teclado Mecánico', 'Teclado mecánico rosa.', 8.99, 'Teclado', 'vendedor3', '/fotos/producto6.jpg', 'enventa'),
+('Monitor Samsung', 'Monitor Full HD de 24 pulgadas.', 14.99, 'Pantalla', 'vendedor1', '/fotos/producto2.jpg', 'enventa'),
+('Ratón', 'Ratón inalámbrico color negro. ', 7.99, 'Ratón', 'vendedor3', '/fotos/producto5.jpg', 'enventa'),
+('Teclado Mecánico', 'Teclado mecánico negro.', 8.99, 'Teclado', 'vendedor3', '/fotos/producto4.jpg', 'enventa'),
 ('Impresora', 'Impresora Canon blanco.', 10.99, 'Impresora', 'vendedor3', '/fotos/producto1.jpg', 'enventa');
 
