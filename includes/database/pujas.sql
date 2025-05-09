5ae1e8f3a2ee4767c24a01d55ecdf218b8ccea57
yayayajia <129692232+yayayajia@users.noreply.github.com>
2025-05-09 15:53:13 +0200

    añadido datos sql

--- includes/database/pujas.sql
+++ includes/database/pujas.sql
@@ -6,3 +6,21 @@ CREATE TABLE Pujas (
     FOREIGN KEY (idSubasta) REFERENCES Subastas(id),
     FOREIGN KEY (idPujador) REFERENCES Usuarios(userid)
 );
+
+INSERT INTO `pujas` (`idSubasta`, `idPujador`, `precio`) VALUES
+(10000, 'elena2', 1002.00),
+(10001, 'elena2', 2010.00),
+(10002, 'elena2', 2400.00),
+(10003, 'elena2', 900.00),
+(10004, 'elena2', 40.00),
+(10000, 'usuarioA', 1010.00),
+(10001, 'usuarioA', 2030.00),
+(10002, 'usuarioA', 2500.00),
+(10003, 'usuarioA', 1000.00),
+(10004, 'usuarioA', 50.00),
+(10000, 'usuarioB', 1200.00),
+(10001, 'usuarioB', 2050.00),
+(10002, 'usuarioB', 2501.00),
+(10003, 'usuarioB', 1003.00),
+(10004, 'usuarioB', 51.00),
+(10000, 'vendedor1', 1300.00);
--- includes/database/usuario.sql
+++ includes/database/usuario.sql
@@ -8,9 +8,12 @@ CREATE TABLE Usuarios (
     rol VARCHAR(50) NOT NULL
 );
 
-INSERT INTO Usuarios (userid, contrasena, email, nombre, apellidos, edad, rol) 
-VALUES
+
 --la contraseña: vendedor1, vendedor2, vendedor3
+INSERT INTO `usuarios` (`userid`, `contrasena`, `email`, `nombre`, `apellidos`, `edad`, `rol`) VALUES
+('elena2', '$2y$10$.LLx5znSjQUISauimdECd.pSmuxyAZQ5kDOybY7vBniWxeTzICqqC', 'elena.rr212@gmail.com', 'elena', 'rodriguez', 23, 'usuario'),
+('usuarioA', '$2y$10$6w26wQWlW6XecjYYAbeEl.7O3nwEyGdelS3w6RESZoViz9gI5WCq2', 'usuarioA@gmail.com', 'usuarioA', 'testeoA', 23, 'usuario'),
+('usuarioB', '$2y$10$wrx2uiWlXdTncECdcgQSFOBEixizDO48CKkxWFbTuPmtquoE.g4Ze', 'usuarioB@gmail.com', 'usuarioB', 'testeoB', 23, 'usuario'),
 ('vendedor1', '$2y$10$XrvoiRarSpb/TB/YldgM4eoQUO.8iW7Xs4MB4QX5Zzd2GC89Gvwqe', 'vendedor1@example.com', 'Juan', 'Pérez', 30, 'usuario'),
 ('vendedor2', '$2y$10$92WPTjQkORMGAoGlu5FvFuUGescNf0ZUL.VvbqGTyxEvSwc7L5fpu', 'vendedor2@example.com', 'María', 'Gómez', 28, 'usuario'),
 ('vendedor3', '$2y$10$2qxmXLlBu.ZuSyb7iPamwuGLa7l24QesyPJ91SbpJVR/F0QIW7fgK', 'vendedor3@example.com', 'Carlos', 'López', 35, 'usuario');
